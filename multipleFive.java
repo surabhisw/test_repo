2df84bda73b45d68bf69c3bd0fd89860bb9c5023
swable <swable@xento.com>
2022-05-04 16:14:28 +0530

    multipleFive file commit

---
+++ multipleFive.java
@@ -0,0 +1,14 @@
+public class multipleFive {
+    public static void main(String[] args) {
+        System.out.println("Table of 5");
+        for(int i=1;i<51;i++)
+        {
+            if(i%5==0)
+            {
+                System.out.println(i);
+
+            }
+        }
+
+    }
+}
